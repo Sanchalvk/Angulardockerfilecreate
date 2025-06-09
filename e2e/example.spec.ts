2f004405b1b70629809a418bdb09bf1523d7de13
Sanchalvk <150319103+Sanchalvk@users.noreply.github.com>
2025-06-09 16:52:06 +0530

    Update example.spec.ts

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,6 @@
-// e2e/example.spec.ts
 import { test, expect } from '@playwright/test';
 
-test('home page should have title', async ({ page }) => {
+test('homepage loads', async ({ page }) => {
   await page.goto('http://localhost:4200');
-  await expect(page).toHaveTitle(/Angular/); // Adjust as needed
+  await expect(page).toHaveTitle(/Angular/); // Adjust this to your actual title
 });
